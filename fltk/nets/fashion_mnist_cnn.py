4fed373d626fbcebb125774c69df3ed2ec7ef16d
arabellastrange <s_degachi@hotmail.co.uk>
2021-10-18 17:43:36 +0200

    high layers high nodes low parameters

--- fltk/nets/fashion_mnist_cnn.py
+++ fltk/nets/fashion_mnist_cnn.py
@@ -12,36 +12,37 @@ class FashionMNISTCNN(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2))
 
-        # self.layer2 = nn.Sequential(
-        #     nn.Conv2d(16, 32, kernel_size=5, padding=2),
-        #     nn.BatchNorm2d(32),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
 
-        # self.layer3 = nn.Sequential(
-        #     nn.Conv2d(32, 64, kernel_size=5, padding=2),
-        #     nn.BatchNorm2d(64),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(2))
-        #
-        # self.layer4 = nn.Sequential(
-        #     nn.Conv2d(64, 128, kernel_size=5, padding=2),
-        #     nn.BatchNorm2d(128),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(2))
-        #
-        # self.flatten = nn.Flatten()
-        #
-        # self.fc = nn.Linear(128, 10)
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=5, padding=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=5, padding=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+
+        self.flatten = nn.Flatten()
+
+        self.fc = nn.Linear(128, 10)
         self.flatten = nn.Flatten()
         self.fc = nn.Linear(3136, 10)
 
     def forward(self, x):
         x = self.layer1(x)
-        # x = self.layer2(x)
-        # x = self.layer3(x)
-        # x = self.layer4(x)
-        # print("size: ", self.flatten(x).shape)
-        x = self.fc(self.flatten(x))
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        print("size: ", self.flatten(x).shape)
+
+        # x = self.fc(self.flatten(x))
 
         return x
