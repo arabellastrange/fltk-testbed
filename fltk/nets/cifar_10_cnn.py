429a10ca987d58957108a2aaa09ece37f4ce3603
arabellastrange <s_degachi@hotmail.co.uk>
2021-10-18 13:14:34 +0200

    correct layers

--- fltk/nets/cifar_10_cnn.py
+++ fltk/nets/cifar_10_cnn.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Cifar10CNN(nn.Module):
 
     def __init__(self):
@@ -12,20 +13,28 @@ class Cifar10CNN(nn.Module):
         self.bn2 = nn.BatchNorm2d(32)
         self.pool1 = nn.MaxPool2d(kernel_size=2)
 
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.pool2 = nn.MaxPool2d(kernel_size=2)
-
-        self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn5 = nn.BatchNorm2d(128)
-        self.conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.bn6 = nn.BatchNorm2d(128)
-        self.pool3 = nn.MaxPool2d(kernel_size=2)
-
-        self.fc1 = nn.Linear(128 * 4 * 4, 128)
-
+        # self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        # self.bn3 = nn.BatchNorm2d(64)
+        # self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        # self.bn4 = nn.BatchNorm2d(64)
+        # self.pool2 = nn.MaxPool2d(kernel_size=2)
+        #
+        # self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        # self.bn5 = nn.BatchNorm2d(128)
+        # self.conv6 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        # self.bn6 = nn.BatchNorm2d(128)
+        # self.pool3 = nn.MaxPool2d(kernel_size=2)
+        #
+        # self.conv7 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        # self.bn7 = nn.BatchNorm2d(256)
+        # self.conv8 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        # self.bn8 = nn.BatchNorm2d(256)
+        # self.pool4 = nn.MaxPool2d(kernel_size=2)
+        # self.fc1 = nn.Linear(256 * 2 * 2, 128)
+        #
+        # self.softmax = nn.Softmax()
+        # self.fc2 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(32 * 16 * 16, 128)
         self.softmax = nn.Softmax()
         self.fc2 = nn.Linear(128, 10)
 
@@ -34,16 +43,22 @@ class Cifar10CNN(nn.Module):
         x = self.bn2(F.relu(self.conv2(x)))
         x = self.pool1(x)
 
-        x = self.bn3(F.relu(self.conv3(x)))
-        x = self.bn4(F.relu(self.conv4(x)))
-        x = self.pool2(x)
+        # x = self.bn3(F.relu(self.conv3(x)))
+        # x = self.bn4(F.relu(self.conv4(x)))
+        # x = self.pool2(x)
+        #
+        # x = self.bn5(F.relu(self.conv5(x)))
+        # x = self.bn6(F.relu(self.conv6(x)))
+        # x = self.pool3(x)
+        #
+        # x = self.bn7(F.relu(self.conv7(x)))
+        # x = self.bn8(F.relu(self.conv8(x)))
+        # x = self.pool4(x)
 
-        x = self.bn5(F.relu(self.conv5(x)))
-        x = self.bn6(F.relu(self.conv6(x)))
-        x = self.pool3(x)
-
-        x = x.view(-1, 128 * 4 * 4)
-
+        # x = x.view(-1, 128 * 4 * 4)
+        # layer 4
+        # x = x.view(-1, 256 * 2 * 2)
+        # x = x.view(-1, 32 * 16 * 16)
         x = self.fc1(x)
         x = self.softmax(self.fc2(x))
 
